68413eeb0b7f6ff80421598fc8e831c7bbc4c0be
Gary M. Silva <garymsilva@outlook.com>
2020-08-21 13:33:11 -0300

    refactor(stats-service): Change sql query to take data from wp_wc_order_stats table

--- src/api/services/stats.service.js
+++ src/api/services/stats.service.js
@@ -1,21 +1,13 @@
 const db = require('../database/connection');
 
 function selectAllStats(callback) {
+
   const query = `
   SELECT\
-    SUM(sales.total_sales) as totalProductsSold,\
-    SUM(price.price) as totalProductRevenue,\
-    AVG(price.price) as averageProductPrice\
-  FROM wp_posts\
-  JOIN (\
-    SELECT post_id, meta_value AS total_sales FROM wp_postmeta WHERE meta_key='total_sales'\
-  ) AS sales\
-    ON sales.post_id=wp_posts.ID\
-  JOIN (\
-    SELECT post_id, meta_value AS price FROM wp_postmeta WHERE meta_key='_price'\
-  ) as price\
-    ON price.post_id=wp_posts.ID\
-  WHERE post_type='product'`;
+    SUM(num_items_sold) AS totalProductsSold,\
+    SUM(total_sales) AS totalProductRevenue,\
+    (SUM(total_sales) / SUM(num_items_sold)) AS averageProductPrice\
+  FROM wp_wc_order_stats`;
 
   db.getConnection().query(query, function (error, result) {
     if (error) {
